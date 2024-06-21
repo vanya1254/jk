2c2df026d4f5ed11bbe7f15e9fadc375309efd36
Ivan <vanya1254@yandex.ru>
2024-06-21 18:13:29 +0300

    feat: add some styles for product

--- app/components/ProductWrapper/index.tsx
+++ app/components/ProductWrapper/index.tsx
@@ -2,13 +2,17 @@
 
 import React, { useState } from "react";
 
+import useWindowSize from "@/app/hooks/useWindowSize";
 import { useAppSelector } from "@/lib/hooks";
 import { productSelector } from "@/lib/features/product/selectors";
 
-import styles from "./ProductWrapper.module.scss";
 import { ButtonCustom } from "../ButtonCustom";
 
+import styles from "./ProductWrapper.module.scss";
+import { SwiperSlider } from "../";
+
 export const ProductWrapper: React.FC = () => {
+  const size = useWindowSize();
   const [isRead, setIsRead] = useState(false);
   const { product } = useAppSelector(productSelector);
 
@@ -16,51 +20,62 @@ export const ProductWrapper: React.FC = () => {
     setIsRead((prev) => !prev);
   };
 
+  const imgs = product.productTemplateExternalPictures.map((img, i) => (
+    <div key={i} className={styles.root__img}>
+      <img src={img} alt={`img ${product.name}`} />
+      <span className={styles.root_counter}>{`[${i + 1}/${
+        product.productTemplateExternalPictures.length
+      }]`}</span>
+    </div>
+  ));
+
   return (
     <section className={styles.root}>
-      <div className={styles.root__imgs}>
-        {product.productTemplateExternalPictures.map((img, i) => (
-          <div className={styles.root__img}>
-            <img key={i} src={img} alt={`img ${product.name}`} />
-            <span
-              className={styles.root_counter}
-            >{`[${i}/${product.productTemplateExternalPictures.length}]`}</span>
+      {size.width >= 1024 ? (
+        <div className={styles.root__imgs}>{imgs}</div>
+      ) : (
+        <SwiperSlider
+          className={styles.root__imgs}
+          children={imgs}
+          gap={0}
+          slidesPerView={1}
+        />
+      )}
+      <div className={styles.root__wrapper}>
+        <div className={styles.root__description}>
+          <h2 className={styles.root__description_name}>{product.name}</h2>
+          <p className={styles.root__description_price}>{`$${(
+            product.priceCents / 100
+          ).toFixed(2)}`}</p>
+          <div className={styles.root__description__info}>
+            <p>{product.story}</p>
+            <div className={styles.root__description_more}>
+              {isRead ? "" : <span>...</span>}
+              <button
+                className={styles.root__description_btn}
+                onClick={onClickMore}
+              >
+                {isRead ? "Read less" : "Read more"}
+              </button>
+            </div>
           </div>
-        ))}
-      </div>
-      <div className={styles.root__description}>
-        <h2 className={styles.root__description_name}>{product.name}</h2>
-        <p className={styles.root__description_price}>{`$${(
-          product.priceCents / 100
-        ).toFixed(2)}`}</p>
-        <div className={styles.root__description_info}>
-          <p>{product.story}</p>
-          <div className={styles.root}>
-            <span>...</span>
-            <button
-              className={styles.root__description_btn}
-              onClick={onClickMore}
-            >
-              {isRead ? "Read less" : "Read more"}
-            </button>
+          <div className={styles.root__color}>
+            <span className={styles.root_subtitle}>Color:</span>
+            <div className={styles.root__color_view}></div>
           </div>
-        </div>
-        <div className={styles.root__color}>
-          <span className={styles.root_subtitle}>Color:</span>
-          <div className={styles.root__color_view}></div>
-        </div>
-        <div className={styles.root__sizesInfo}>
-          <span className={styles.root_subtitle}>Sizes (us):</span>
-          <div className={styles.root__sizesInfo__sizes}>
-            {product.sizeRange.map((size, i) => (
-              <ButtonCustom
-                onClick={undefined}
-                text={size.toString()}
-              ></ButtonCustom>
-            ))}
+          <div className={styles.root__sizesInfo}>
+            <span className={styles.root_subtitle}>Sizes (us):</span>
+            <div className={styles.root__sizesInfo__sizes}>
+              {product.sizeRange.map((size, i) => (
+                <ButtonCustom
+                  onClick={undefined}
+                  text={size.toString()}
+                ></ButtonCustom>
+              ))}
+            </div>
           </div>
+          <button className={styles.root_btn}>Add to bag</button>
         </div>
-        <button className={styles.root_btn}>Add to bag</button>
       </div>
     </section>
   );
